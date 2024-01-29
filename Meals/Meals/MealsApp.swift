7573d970e37f60f132de52653ad988c19ace62b5
DipenPanchasara <dipenapanchasara@gmail.com>
2024-01-29 20:38:25 +0000

    Created a base project for Meals.

---
+++ Meals/Meals/MealsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealsApp.swift
+//  Meals
+//
+//  Created by Dipen Panchasara on 29/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct MealsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
