b924665158edef4771fa17a2a9c5f041b9186823
DipenPanchasara <dipenapanchasara@gmail.com>
2024-02-20 17:29:49 +0000

    Added custom back button in navigation bar through view modifier.

--- Meals/Meals/Presentation/ViewModifiers/ToolbarStyleModifier.swift
+++ Meals/Meals/Presentation/ViewModifiers/ToolbarStyleModifier.swift
@@ -9,6 +9,9 @@ import SwiftUI
 
 struct ToolbarStyleModifier: ViewModifier {
   let color: Color
+  let showBackButton: Bool
+  var onTap: (() -> Void)?
+
   func body(content: Content) -> some View {
     content
       .toolbarColorScheme(
@@ -23,11 +26,27 @@ struct ToolbarStyleModifier: ViewModifier {
         .visible,
         for: .navigationBar
       )
+      .navigationBarBackButtonHidden()
+      .toolbar {
+        if showBackButton {
+          ToolbarItem(placement: .topBarLeading) {
+            Button("", systemImage: "arrow.left.circle") {
+              onTap?()
+            }
+          }
+        }
+      }
   }
 }
 
 extension View {
-  func toolBarStyle(color: Color = .pink) -> some View {
-    modifier(ToolbarStyleModifier(color: color))
+  func toolBarStyle(color: Color = .pink, showBackButton: Bool = false, onTap: (() -> Void)? = nil) -> some View {
+    modifier(
+      ToolbarStyleModifier(
+        color: color,
+        showBackButton: showBackButton,
+        onTap: onTap
+      )
+    )
   }
 }
